240a1bbf66575db6de42717c9b045306c8b1d81d
geekamita <amitasingh5661@gmail.com>
2024-09-02 11:35:22 +0530

    Added Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 import { Button } from "@/components/ui/button";
+import { LoginButton } from "@/components/auth/login_button";
 
 export default function Home() {
   return (
-    <Button size="lg">Click me</Button>
+    <main className="flex h-full flex-col justify-center items-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-gray-400 to-gray-950">
+      <div className="space-y-6 text-center">
+        <h1 className="font-semibold text-8xl text-[#f0ebeb] drop-shadow-md">
+          Auth
+        </h1>
+        <p className="text-2xl text-[#f0ebeb]">
+          An authentication service
+        </p>
+        <div>
+          <LoginButton>
+            <Button variant="outline" size="xl">
+              Sign In
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+    </main>
   );
 }
